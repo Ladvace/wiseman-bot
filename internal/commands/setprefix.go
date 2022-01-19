3ca08dd3fc8f50044aa1c2dc64076655828f0c9e
Gianmarco <cavallogianmarco@gmail.com>
2022-01-19 20:11:12 +0100

    setPrefix command

--- internal/commands/setprefix.go
+++ internal/commands/setprefix.go
@@ -1,9 +1,15 @@
 package commands
 
 import (
+	"context"
+	"fmt"
 	"wiseman/internal/discord"
+	"wiseman/internal/servers"
+	"wiseman/internal/shared"
 
 	"github.com/bwmarrin/discordgo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -15,10 +21,35 @@ func init() {
 		Usage:       "This is a usage",
 	})
 
-	discord.Commands["setPrefix"] = Setprefix
+	discord.Commands["setprefix"] = Setprefix
 }
 
-func Setprefix(s *discordgo.Session, m *discordgo.MessageCreate, mongo *mongo.Client, args []string) error {
+func Setprefix(s *discordgo.Session, m *discordgo.MessageCreate, client *mongo.Client, args []string) error {
+
+	ctx := context.TODO()
+	if len(args) == 0 {
+		return nil
+	}
+	prefix := args[0]
+	collection := client.Database(shared.DB_NAME).Collection(shared.SERVERS_INFIX)
+	server := servers.Get(m.GuildID)
+
+	result, err := collection.UpdateOne(
+		ctx,
+		bson.M{"serverid": m.GuildID},
+		bson.D{
+			// primitive.E{Key: "$set", Value: bson.M{"guildprefix": prefix}},
+			primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: "guildprefix", Value: prefix}}},
+		},
+	)
+
+	fmt.Printf("Updated %v Documents!\n", result.ModifiedCount)
+	server.GuildPrefix = prefix
+	servers.Upsert(m.GuildID, server)
+
+	if err == nil {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Prefix set to %#v", prefix))
+	}
 
 	return nil
 }
