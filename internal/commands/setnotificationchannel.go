e2a38e0a192856d5891b0d36ba7d08f5af29ed4d
Gianmarco cavallo <cavallogianmarco@gmail.com>
2022-02-12 12:07:38 +0100

    setnotificationchannel

---
+++ internal/commands/setnotificationchannel.go
@@ -0,0 +1,73 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"wiseman/internal/db"
+	"wiseman/internal/discord"
+
+	"github.com/bwmarrin/discordgo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func init() {
+	Helpers = append(Helpers, Helper{
+		Name:        "setNotificationchannel",
+		Category:    "This is a category",
+		Description: "This is a descriptio",
+		Usage:       "This is a usage",
+	})
+
+	discord.Commands["setnotificationchannel"] = Setnotificationchannel
+}
+
+func Setnotificationchannel(s *discordgo.Session, m *discordgo.MessageCreate, args []string) error {
+
+	ctx := context.TODO()
+	if len(args) == 0 {
+		return nil
+	}
+	channelId := args[0]
+	collection := db.SERVERS_DB
+	server := db.GetServerByID(m.GuildID)
+
+	if channelId == "null" {
+		_, err := collection.UpdateOne(
+			ctx,
+			bson.M{"serverid": m.GuildID},
+			bson.D{
+				primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: "notificationchannel", Value: ""}}},
+			},
+		)
+
+		server.NotificationChannel = ""
+		db.UpsertServerByID(m.GuildID, server)
+
+		if err == nil {
+			s.ChannelMessageSend(m.ChannelID, "Notification Channel has been reset!")
+		}
+	}
+
+	channel, err := s.Channel(channelId)
+	if err != nil {
+		return nil
+	}
+
+	_, err = collection.UpdateOne(
+		ctx,
+		bson.M{"serverid": m.GuildID},
+		bson.D{
+			primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: "notificationchannel", Value: channelId}}},
+		},
+	)
+
+	server.NotificationChannel = channelId
+	db.UpsertServerByID(m.GuildID, server)
+
+	if err == nil {
+		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Notification Channel set to %#v", channel.Name))
+	}
+
+	return nil
+}
